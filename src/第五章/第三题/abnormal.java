10c580f8fec6a40fb7dea1fdc4bd0e4dd6fe6c7d
84273 <842736416@qq.com>
2019-09-20 13:28:57 +0800

    作业

--- src/第五章/第三题/abnormal.java
+++ src/第五章/第三题/abnormal.java
@@ -1,13 +1,13 @@
 package 第五章.第三题;
-
+import org.apache.log4j.Logger;
 public class abnormal {
+    private static final Logger logger = Logger.getLogger(abnormal.class);
     public static void main(String[] args) {
         try {
             int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
             System.out.println(array[9]);
         }catch (Exception e){
-            System.err.println(e.getMessage());
-            e.printStackTrace();
+            logger.error(e.getMessage());
         }finally {
             System.out.println("程序结束！！！");
         }
